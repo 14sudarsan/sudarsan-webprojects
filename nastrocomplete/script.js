d606f0bb094016536dd56d662d30ca16e0c50fa0
14sudarsan <131959397+14sudarsan@users.noreply.github.com>
2024-01-26 10:08:27 -0800

    Add files via upload

---
+++ nastrocomplete/script.js
@@ -0,0 +1,13 @@
+var navbarlinks=document.querySelector(".navbar-links")
+var sidenavbar=document.querySelector(".side-bar")
+var navbarlink=document.querySelector(".side-navbar-link")
+function visible(){
+    sidenavbar.style.left="0";
+    
+    
+
+}
+function visibleclose(){
+    sidenavbar.style.left="-60%";
+
+}
